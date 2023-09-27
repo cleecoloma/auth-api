b83e46c2d9c6940f489b4f6769a61f62e8b520c4
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-27 16:47:05 -0700

    added starter code

---
+++ src/models/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const userModel = require('../auth/models/users.js');
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesModel = require('./clothes/model.js');
+const foodModel = require('./food/model.js');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory;';
+
+const sequelize = new Sequelize(DATABASE_URL);
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  users: userModel(sequelize, DataTypes),
+  food: new Collection(food),
+  clothes: new Collection(clothes),
+};
